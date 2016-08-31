050791a61fe68105f9f345a3bdceb5a51f5342bf
unknown <matso354@PC1-4.helix.ida.liu.se>
2016-08-31 17:24:34 +0200

    part 1 lab 1

--- UnitTestProject1/TriangleTester.cs
+++ UnitTestProject1/TriangleTester.cs
@@ -16,12 +16,60 @@ namespace TriangleTest
             Triangle = new Triangle();
         }
 
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Triangle.Sides = null;
+        }
+        
+
         [TestMethod]
         public void TestInvalid()
         {
             Triangle.Sides = new int[] {1,2,3};
             Action testInvalid = () => Triangle.DetectType();
             testInvalid.ShouldThrow<InvalidTriangleException>();
+        }
+
+        [TestMethod]
+        public void TestWrongArraySize()
+        {
+            Triangle.Sides = new int[] { 2, 2};
+            Action testInvalid = () => Triangle.DetectType();
+            testInvalid.ShouldThrow<InvalidTriangleException>();
+        }
+
+
+        [TestMethod]
+        public void TestEquilateral()
+        {
+            Triangle.Sides = new int[] { 1, 1, 1 };
+            Assert.AreEqual(Triangle.TriangleType.EQUILATERAL, Triangle.DetectType());
+        }
+
+        [TestMethod]
+        public void TestIsosceles()
+        {
+            Triangle.Sides = new int[] { 2, 2, 1 };
+            Assert.AreEqual(Triangle.TriangleType.ISOSCELES, Triangle.DetectType());
+        }
+
+        [TestMethod]
+        public void TestScalene()
+        {
+            Triangle.Sides = new int[] { 2, 3, 4 };
+            Assert.AreEqual(Triangle.TriangleType.SCALENE, Triangle.DetectType());
+        }
+
+
+
+        [TestMethod]
+        public void TestNegativeInput()
+        {
+            Triangle.Sides = new int[] { -2, 3, 4 };
+            Action testNegativeInput = () => Triangle.DetectType();
+            testNegativeInput.ShouldThrow<InvalidTriangleException>();
         }
 
 
